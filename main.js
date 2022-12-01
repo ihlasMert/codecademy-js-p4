c85891dc2ada48431471b855e9906b0ab53d5ef2
İHLASMERT <ihlas1mert@gmail.com>
2022-12-01 13:07:34 +0300

    added js functions

---
+++ main.js
@@ -0,0 +1,12 @@
+let view = document.getElementById("view-button");
+let close = document.getElementById("close-button");
+let codey = document.getElementById("codey");
+
+let open = function () {
+  codey.style.display = "block";
+  close.style.display = "block";
+};
+let hide = function () {
+  codey.style.display = "none";
+  close.style.display = "none";
+};
